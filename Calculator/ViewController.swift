3fadac9f2f1fb401e97bcceb49d853db50bb38ba
Hector Mendoza <hectormendoza@byui.edu>
2018-10-17 08:41:58 -0400

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Hector Mendoza on 10/17/18.
+//  Copyright © 2018 Hector Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
