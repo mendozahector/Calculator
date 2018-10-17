6e60ce0d23b56199d64f6cd57e76cb73cb785a7f
Hector Mendoza <hectormendoza@hectors-macbook-pro.local>
2018-10-17 16:22:13 -0400

    Refactoring with computed properties.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     private var isFinishedTypingNumber: Bool = true
-//    private var displayValue: Double {
-//
-//    }
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            
+            return number
+        }
+        
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,32 +35,26 @@ class ViewController: UIViewController {
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         isFinishedTypingNumber = true
         
-        guard let number = Double(displayLabel.text!) else {
-            fatalError("Cannot convert display label text to a Double")
-        }
-        
         if let calcMethod = sender.currentTitle {
             switch calcMethod {
             case "+/-":
-                displayLabel.text = String(number * -1)
+                displayValue *= -1
             case "AC":
                 displayLabel.text = "0"
             case "%":
-                displayLabel.text = String(number / 100)
+                displayValue /= 100
             default:
-                displayLabel.text = String(number)
+                displayLabel.text = String(displayValue)
             }
         }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         if let numValue = sender.currentTitle {
-            if displayLabel.text!.contains(".") && numValue == "." {
-                    //we already have a decimal
-            } else {
+            if !displayLabel.text!.contains(".") || numValue != "." {
                 if isFinishedTypingNumber {
                     displayLabel.text = numValue
-                        
+                    
                     isFinishedTypingNumber = false
                 } else {
                     displayLabel.text = displayLabel.text! + numValue
